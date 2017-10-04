4a00b382543c4ef31e9dbf19e03bdca5a52ff0c6
fire855 <thefire855@gmail.com>
2017-10-04 23:12:07 +0200

    Move everything to vendor
    Conflicts:
    	vendor/bin/mxg2320d
    	vendor/lib/libvcodec_cap.so
    	vendor/lib64/libvcodec_oal.so

--- thor-vendor-blobs.mk
+++ thor-vendor-blobs.mk
@@ -1,11 +1,3 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/thor/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/thor/lib64,system/lib64)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/thor/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/thor/etc,system/etc)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/thor/usr,system/usr)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/thor/vendor,system/vendor)
